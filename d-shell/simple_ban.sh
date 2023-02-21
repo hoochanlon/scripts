dae0252d2418568490e325800fc3cdad18f23945
菠萝包趣多多 <hoochanlon@outlook.com>
2023-02-21 10:11:31 +0800

    Update simple_ban.sh

--- d-shell/simple_ban.sh
+++ d-shell/simple_ban.sh
@@ -3,30 +3,37 @@ yum install fail2ban-firewalld fail2ban-systemd -y
 yum -y install git python3
 
 echo -e "安装及配置fail2ban: 两次输入错误密码，封禁IP永久。\n"
+# 备份原始文件
+mkdir -p /etc/bak/fail2ban_conf/ && cp -p /etc/fail2ban/jail.conf /etc/bak/fail2ban_conf/
+# 看来得做个定义输出，插入替换式使用。
 echo -e \
 "
 [DEFAULT]
-ignoreip=127.0.0.1 ＃用于指定哪些地址(IP/域名等)可以忽路fai12ban防御，空格分隔
-findtime=30 ＃检测扫描行为的时间窗口（单位：秒），和maxretry结合使用，30秒内失败2次即封禁
-maxretry=2 ＃检测扫描行为的次数，和findtime结合使用，30秒内失败2次即封禁
-bantime= -1 ＃封禁该ip的时间（单位：秒），-1为永久封禁
-banaction=iptables-allports #封禁该ip的端口
+ignoreip = 127.0.0.1 ＃用于指定哪些地址(IP/域名等)可以忽路fai12ban防御，空格分隔
+maxretry = 3   #检测扫描行为的次数，和findtime结合使用，30秒内失败3次即封禁
+findtime  = 10 #在该时间段内超过尝试次数会被ban掉
+bantime = -1  #屏蔽时间，默认是秒，-1为永久封禁
 
-[sshd]
-enabled=true #启用ssh扫描判断器
-port=22 ＃ssh的端口，如更换过ssh的默认端口请更改成相应端口
-filter=sshd #启用ssh扫描判断器
-logpath=/var/log/auth.log #系统行为记录日志，一般无需改动
+# “ssh-iptables”为模块配置名称，命令用法 fail2ban-client status + 模块名
+# 如：fail2ban-client status  ssh-iptables
+
+[ssh-iptables] 
+enabled = true
+filter = sshd
+action = iptables[name=SSH, port=22, protocol=tcp] # action - 指定被命中 IP 主机地址禁止其访问的行为
+logpath  = /var/log/secure #系统行为记录日志，一般无需改动
 " >> /etc/fail2ban/jail.local
+
 
 echo -e "加入守护进程，已设定自启，fail2ban现已启动 \n"
 systemctl enable fail2ban.service && systemctl start fail2ban.service
-echo -e "查看ban IP后续可使用：cat /var/log/fail2ban.log "
+echo -e "查看ban IP后续可使用： fail2ban-client status ssh-iptables"
 
-# 调试代码
-# rm -rf /.ssh/authorized_keys
-
+rm -rf $0
 # 报错调整参考：
+# [stackoverflow-cant-enable-fail2ban-jail-sshd](https://stackoverflow.com/questions/42320994/cant-enable-fail2ban-jail-sshd)
 # [fail2ban/issues/3462](https://github.com/fail2ban/fail2ban/issues/3462)
+# [learnku-如何使用 fail2ban 来防范 SSH 暴力破解？](https://learnku.com/server/t/36233)
 # [豆瓣-CentOS7 - 安装配置Fail2ban教程](https://www.douban.com/note/626688457/?_i=6892452FeAKN2V)
 # [csdn-centos8安装Nginx时报错 nginx.service: Unit cannot be reloaded becau lines 1-5](https://blog.csdn.net/m0_47748508/article/details/119203994)
+# [How to Install Fail2ban on Rocky Linux and AlmaLinux](https://www.tecmint.com/install-fail2ban-rocky-linux-almalinux/)
