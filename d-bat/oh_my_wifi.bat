2e2b4dc32a9d1f65671eb4fc531c5ecbe8db6416
123456 <hoochanlon@outlook.com>
2023-04-12 14:55:21 +0800

    asd

--- d-bat/oh_my_wifi.bat
+++ d-bat/oh_my_wifi.bat
@@ -1,10 +1,7 @@
 @REM  特权提升，懒得右键或以管理员启动
-@REM %1 C:\Windows\SysWOW64\mshta.exe vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-@REM @cd /d "%~dp0"
+%1 C:\Windows\SysWOW64\mshta.exe vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+@cd /d "%~dp0"
 
-@REM 特权提升可行性研究
-@REM curl -L  https://ghproxy.com/https://github.com/hoochanlon/ihs-simple/raw/main/d-bat/oh_my_wifi.bat|cmd
-@REM C:\Windows\SysWOW64\mshta.exe vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 :: ^& echo curl -L https://ghproxy.com/https://github.com/hoochanlon/ihs-simple/raw/main/d-bat/oh_my_wifi.bat ^| cmd && pause","","runas",1)(window.close)&&exit
 @REM 输出空行
 @ECHO/
 @echo 该获取所有WiFi密码，仅供研究学习使用
