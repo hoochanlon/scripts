a5c4b7cf13c8c4e8bdeedf61e99ef6f8ec851120
123456 <hoochanlon@outlook.com>
2023-05-11 16:03:34 +0800

    Update get_resou_today.py

--- d-python/get_resou_today.py
+++ d-python/get_resou_today.py
@@ -10,6 +10,7 @@ import jieba.posseg as pseg
 import json
 import urllib.request
 
+# 确定保存文本格式用的。
 def get_formatted_time():
     """
     获取格式化后的当前时间
@@ -18,15 +19,7 @@ def get_formatted_time():
     now = datetime.now()
     return now.strftime('%Y-%m-%d')
 
-
-def create_dir_if_not_exists(directory: str):
-    """
-    如果目录不存在，则创建它
-    :param directory: 目录路径
-    """
-    os.makedirs(directory, exist_ok=True)
-
-
+# 解析新闻函数
 def get_news_from_url(url: str):
     """
     从指定的 URL 抓取热搜新闻
@@ -54,8 +47,11 @@ def delete_empty_rows(sheet_name: str, wb: Workbook):
     删除指定工作表中的空行
     :param sheet_name: 工作表名称
     :param wb: Excel 工作簿对象
+    :param None 关键字
+    https://notes-by-yangjinjie.readthedocs.io/zh_CN/latest/python/05-modules/openpyxl.html?highlight=openpyxl
     """
     ws = wb[sheet_name]
+    # 迭代行所用的内置函数 ws.iter_rows()
     for row in ws.iter_rows():
         if all(cell.value is None for cell in row):
             ws.delete_rows(row[0].row)
@@ -77,22 +73,30 @@ def calculate_average_index_and_sentiment_score(sheet_name: str, wb: Workbook):
         for cell in row:
             if cell.value is not None:
                 news_str += str(cell.value)
+        # 功能挺多，见：https://github.com/isnowfy/snownlp
         s = SnowNLP(news_str)
         sentiment_score = s.sentiments
         sentiment_score_list.append(sentiment_score)
+        # row[2] 是指每一行中的第三个单元格，也就是第三列。
         total_index += row[2].value
         count += 1
         ws.cell(row=row[0].row, column=4, value=sentiment_score)
+    # 每张表的平均指数与情感得分
     return (total_index / count, sum(sentiment_score_list) / len(sentiment_score_list))
 
 # 统计词频
-def calculate_word_count(sheet_names: list, wb: Workbook, stopwords_file: str):
+def calculate_word_count(sheet_names: list, wb: Workbook):
     """
     计算工作表中出现最多的20个单词，将结果写入新的工作表中
     :param sheet_names: 工作表名称
     :param wb: Excel 工作簿对象
     :param stopwords_file: 停用词文件路径
+    停用词是指在自然语言中使用频率很高，
+    但通常不具有实际含义或对文本分析任务没有太大帮助的单词，如“的”，“了”等。
     """
+    
+    # 停用词文件
+    stopwords_file = 'https://ghproxy.com/https://raw.githubusercontent.com/goto456/stopwords/master/cn_stopwords.txt'
     # 请求停用词库
     response = requests.get(stopwords_file)
     stopwords = response.content.decode('utf-8').split('\n')
@@ -119,10 +123,11 @@ def calculate_word_count(sheet_names: list, wb: Workbook, stopwords_file: str):
                  # 忽略长度为 0 或 1 的字符串
                 if len(word) <= 1:
                     continue
+                # 去除数值噪声
                 if not(word.isdigit() or (word.replace('w', '').replace('.', '').isdigit())):
                     new_words.append(word)
             words = new_words
-
+            # 更新词库集
             word_count.update(words)
 
     # 去掉停用词
@@ -130,59 +135,24 @@ def calculate_word_count(sheet_names: list, wb: Workbook, stopwords_file: str):
         if word in stopwords:
             del word_count[word]
 
-    # 取出出现最多的50个词
+    # 取出出现最多的30个词
     top_words = word_count.most_common(30)
-
     # 创建一个新的工作表
     ws = wb.create_sheet(title='词频统计')
-
-    # 将出现最多的20个单词及其词频逐行写入新的工作表中
+    # 添加行
     ws.append(['排名', '词语', '词频'])
-    for i, (word, freq) in enumerate(top_words, 1):
+    # 从1开始计数，而非0开始排名
+    for i, (word, freq) in enumerate(top_words,1):
         ws.append([i, word, freq])
 
 
-# 写入数据
-def write_news_to_sheet(news_list: list, sheet_name: str, wb: Workbook):
-    """
-    将新闻列表写入到 Excel 工作表中
-    :param news_list: 新闻列表
-    :param sheet_name: 工作表名称
-    :param wb: Excel 工作簿对象
-    """
-    ws = wb.create_sheet(title=sheet_name)
-
-    row = []
-    for i, item in enumerate(news_list, start=1):
-        if i >= 156: # 只抽取50组数据（如果索引大于 156 则跳过写入操作，因为微博热榜界面有多余索引行）
-            continue  
-        if i % 3 == 1:
-            item = item.replace("、", "")
-        row.append(item)
-        if i % 3 == 0:
-            ws.append(row)
-            row = []
-
-    for row in ws.iter_rows(min_row=2, min_col=1):
-        for cell in row:
-            if cell.column == 1 or cell.column == 3:
-                if isinstance(cell.value, str) and not cell.value.isnumeric():
-                    # 去除字符串中的 '[置顶]' 字符，185为2、3、4排名的平均值（实时）
-                    cell.value = cell.value.replace('[置顶]', '185w') 
-                if isinstance(cell.value, str) and cell.value.isnumeric():
-                    cell.value = int(cell.value)
-                elif isinstance(cell.value, str):
-                    cell.value = float(cell.value.replace('w', ''))
-
-    ws.cell(row=1, column=3, value='指数（万）')
-    ws.cell(row=1, column=4, value='情感得分')
-    
-
+# 2023.5.9 新增分类功能
 def write_category_to_sheet(sheet_name: str, wb: Workbook):
     """
     将新闻事件的关键词分类信息写入到 Excel 工作表中的第五列中
     :param sheet_name: 工作表名称
     :param wb: Excel 工作簿对象
+    :jieba分词：https://github.com/fxsjy/jieba
     """
 
     # 调用在线分类字典 json
@@ -193,16 +163,21 @@ def write_category_to_sheet(sheet_name: str, wb: Workbook):
     # 解析JSON数据
     category_keywords = json.loads(json_data)
 
+    # 从当前sheet开始
     ws = wb[sheet_name]
     for row in ws.iter_rows(min_row=2, min_col=1, max_col=4):
         title_str = ''
         for cell in row:
             if cell.value is not None:
                 title_str += str(cell.value)
+
+        # 将标题字符串分词并转换为列表数组
         words = pseg.cut(title_str)
         category = ''
         for word, flag in words:
-            if flag.startswith('n'):
+            # 内置的字符串方法，用于检查前缀指定开头
+            # if flag.startswith('n'):
+            # key keywords 关键字，.items() 检索键值对
                 for key, keywords in category_keywords.items():
                     if word in keywords:
                         category = key
@@ -212,12 +187,52 @@ def write_category_to_sheet(sheet_name: str, wb: Workbook):
         if not category:
             category = '其他'
         ws.cell(row=row[0].row, column=5, value=category)
+
+
+# 将已解析网页排版的数据，按规则写入到xlsx，行列对称，条理分明。
+def write_news_to_sheet(news_list: list, sheet_name: str, wb: Workbook):
+    """
+    将新闻列表写入到 Excel 工作表中
+    :param news_list: 新闻列表
+    :param sheet_name: 工作表名称
+    :param wb: Excel 工作簿对象
+    :cell.value.isnumeric() 表示当前字符串是否能表示为一个数字
+    :isinstance(cell.value, str) 表示当前值是否字符串
+    """
+    ws = wb.create_sheet(title=sheet_name)
+
+    row = []
+    for i, item in enumerate(news_list, start=1):
+        if i >= 156: # 抽取50组数据（如果索引大于 156 是关于微博的其他介绍文章）
+            continue  
+        if i % 3 == 1: #  # 索引从0开始，即2%3，往后是新组数据。
+            item = item.replace("、", "")
+        row.append(item)
+        if i % 3 == 0: # 取模被整除，3列一组，被整除说明已到3列，换行。
+            ws.append(row)
+            row = []
+    
+    # 从第二行开始迭代每一行数据
+    for row in ws.iter_rows(min_row=2, min_col=1):
+        for cell in row:
+            if cell.column == 1 or cell.column == 3:
+                # 实例判断，如果是返回 True，否则返回 False。
+                if isinstance(cell.value, str) and not cell.value.isnumeric():
+                    # 去除字符串中的 '[置顶]' 字符，185为2、3、4排名的平均值（实时）
+                    cell.value = cell.value.replace('[置顶]', '185w') 
+                if isinstance(cell.value, str) and cell.value.isnumeric():
+                    cell.value = int(cell.value)
+                elif isinstance(cell.value, str):
+                    cell.value = float(cell.value.replace('w', ''))
+
+    ws.cell(row=1, column=3, value='指数（万）')
+    ws.cell(row=1, column=4, value='情感得分')
     ws.cell(row=1, column=5, value='分类')
+    
+
 
 def main():
-    default_dir = os.path.join(os.path.expanduser("~"), "Desktop")
-    save_path_xlsx_file = os.path.join(default_dir,
-                                       "resoubang_{}.xlsx".format(get_formatted_time()))
+
     urls = ['http://resou.today/art/11.html', 'http://resou.today/art/22.html','http://resou.today/art/6.html']
     sheet_names = ['今日头条热榜', '抖音时事热榜', '微博热搜']
 
@@ -225,24 +240,27 @@ def main():
 
     for url, sheet_name in zip(urls, sheet_names):
         news_list = get_news_from_url(url)
+        # 写入网页解析的数据到xlsx
         write_news_to_sheet(news_list, sheet_name, wb)
+        # 删除操作留下的空行
         delete_empty_rows(sheet_name, wb)
-        
+        # 分类
         write_category_to_sheet(sheet_name, wb)
+        # 词频统计
+        calculate_word_count(sheet_names, wb)
+        # 统计平均指数、各表平均情感值
         average_index, sentiment_score = calculate_average_index_and_sentiment_score(sheet_name, wb)
         print(f'{sheet_name} 平均指数:{average_index:.2f} 情感得分: {sentiment_score:.2f}')
-       
     
+    # 进行跨平台处理保存路径
+    default_dir = os.path.join(os.path.expanduser("~"), "Desktop")
+    # 拼接
+    save_path_xlsx_file = os.path.join(default_dir, "resoubang_{}.xlsx".format(get_formatted_time()))
     # 删除空表
     default_sheet = wb['Sheet'];wb.remove(default_sheet)
-
-    # 停用词文件
-    stopwords_file = 'https://ghproxy.com/https://raw.githubusercontent.com/goto456/stopwords/master/cn_stopwords.txt'
-    # 词频统计
-    calculate_word_count(sheet_names, wb, stopwords_file)
-
+    # 保存为指定文件
     wb.save(save_path_xlsx_file)
 
-
+# 如果当前模块是被其他模块导入的，则该条件语句下面的代码将不会被执行。
 if __name__ == '__main__':
     main()
