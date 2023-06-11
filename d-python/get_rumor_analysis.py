025655eb5272d493a6140bccb70866d6f8b88404
123456 <hoochanlon@outlook.com>
2023-06-12 01:06:02 +0800

    Update get_rumor_analysis.py

--- d-python/get_rumor_analysis.py
+++ d-python/get_rumor_analysis.py
@@ -15,6 +15,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
  
 
 # 分类
