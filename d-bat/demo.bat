013bdda0ee3fdfe4969ae312ec3ec699ddd456b9
Preston Duran <hoochanlon@outlook.com>
2023-06-15 21:58:37 +0800

    Update demo.bat

--- d-bat/demo.bat
+++ d-bat/demo.bat
@@ -205,3 +205,18 @@ rem 连接网络打印机 通用
 @REM 已安装惠普打印机驱动，特例测试
 @REM rundll32 printui.dll,PrintUIEntry /if /b ""  /r "IP_%printerIP%" /m "HP Universal Printing PCL 6"
 
+echo 正在与 %printerHost% 共享打印主机建立连接
+@REM 用户名abc 密码123456 /persistent:yes 永久保存
+net use \\%printerHost% /user:abc 123456 /persistent:yes
+
+echo\
+echo 正在连接打印机，并安装驱动
+@REM  /in 参数表示安装打印机驱动程序，/n 参数指定打印机名称，
+@REM /z 参数表示将打印机设置为默认打印机，/q 参数表示安静模式执行
+rundll32 printui.dll,PrintUIEntry /y /in /n "\\%printerHost%\HP LaserJet MFP M232-M237 PCLmS" /q
+
+@REM color 0a
+@REM timeout /t 30
+@REM color
+echo\
+pause
