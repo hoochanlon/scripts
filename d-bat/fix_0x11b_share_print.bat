5c9ada549c378c7fb828ad97da1a7df1e1ac3c21
123456 <hoochanlon@outlook.com>
2023-07-18 15:33:10 +0800

    Add files via upload

---
+++ d-bat/fix_0x11b_share_print.bat
@@ -0,0 +1,19 @@
+@%1 C:\Windows\SysWOW64\mshta.exe vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+@cd /d "%~dp0"
+@echo off
+@REM https://www.technipages.com/how-to-fix-windows-printer-error-0x0000011b/
+
+@reg add "HKLM\SYSTEM\CurrentControlSet\Control\Print"  /v "RpcAuthnLevelPrivacyEnabled" /t REG_DWORD /d 0 /f
+
+@wusa /uninstall /kb:5005565 /quiet
+@wusa /uninstall /kb:5005566 /quiet
+@wusa /uninstall /kb:5005568 /quiet
+@wusa /uninstall /kb:5022497 /quiet
+@wusa /uninstall /kb:5012170 /quiet
+@wusa /uninstall /kb:5023706 /quiet
+@wusa /uninstall /kb:5007186 /quiet
+
+@net stop spooler
+@del /f /s /q C:\Windows\System32\spool\PRINTERS\*
+@net start spooler
+@pause
