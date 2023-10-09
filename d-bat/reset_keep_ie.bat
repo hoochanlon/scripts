7d13a7ad4efc18df22f82152e57d27fb51c05bdd
123456 <hoochanlon@outlook.com>
2023-10-09 19:18:25 +0800

    Update and rename old_keep_ie.bat to reset_keep_ie.bat

---
+++ d-bat/reset_keep_ie.bat
@@ -0,0 +1,13 @@
+@REM  防IE劫持edge修改回默认EDGE
+
+@echo off
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Enable Browser Extensions" /f
+reg delete "HKCU\Software\Microsoft\Edge\IEToEdge" /v "UpsellDisabled" /f
+reg delete "HKCU\Software\Microsoft\Edge\IEToEdge" /v "RedirectionMode" /f
+reg delete "HKCU\Software\Microsoft\Edge\IEToEdge" /v "QuietExpirationTime" /f
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_DISABLE_IE11_SECURITY_EOL_NOTIFICATION" /v "iexplore.exe" /f
+pause
+
+
+pause
+del %0
