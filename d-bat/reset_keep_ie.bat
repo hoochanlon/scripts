a1cb3262a5ec17ddc958e61c0a5a6896ff2de6e1
123456 <hoochanlon@outlook.com>
2023-10-09 19:20:49 +0800

    1

--- d-bat/reset_keep_ie.bat
+++ d-bat/reset_keep_ie.bat
@@ -6,8 +6,6 @@ reg delete "HKCU\Software\Microsoft\Edge\IEToEdge" /v "UpsellDisabled" /f
 reg delete "HKCU\Software\Microsoft\Edge\IEToEdge" /v "RedirectionMode" /f
 reg delete "HKCU\Software\Microsoft\Edge\IEToEdge" /v "QuietExpirationTime" /f
 REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_DISABLE_IE11_SECURITY_EOL_NOTIFICATION" /v "iexplore.exe" /f
-pause
-
 
 pause
 del %0
