eeac981a432f3b7a64005493a860c43ff2727731
123456 <hoochanlon@outlook.com>
2024-04-11 13:33:55 +0800

    yes

--- d-bat/clear_nullify_startup_reg.bat
+++ d-bat/clear_nullify_startup_reg.bat
@@ -1,9 +1,18 @@
-@REM 问题追溯
-@REM https://answers.microsoft.com/zh-hans/windows/forum/all/%E4%BD%A0%E5%A5%BD-%E8%AF%B7%E9%97%AE/c65a02d7-3e09-4c53-9761-b59ecbea3513
-@REM 删除注册表项
-@REM reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v ProtonVPN /f
-
 @echo off
+
+set "regPath=HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run"
+
+@REM 将路径添加到注册表中的 LastKey 值
+@REM LastKey 是其中的一个值，用于存储上次在注册表编辑器中访问的注册表路径。
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit" /v LastKey /t REG_SZ /d "%regPath%" /f
+
+REM 提示用户是否打开注册表 （archon1 的建议）
+choice /c YN /m "是否打开注册表（高级）定位到相关项"
+if errorlevel 2 (
+    echo 用户选择不打开注册表编辑器。
+) else (
+    start regedit
+)
 
 :loop
 @REM echo 输入要删除的注册表值名称，或输入 'exit' 退出:
@@ -23,3 +32,11 @@ goto :loop
 
 :end
 echo 脚本已退出。
+
+
+@REM 脚本过程
+@REM 问题追溯
+@REM https://answers.microsoft.com/zh-hans/windows/forum/all/%E4%BD%A0%E5%A5%BD-%E8%AF%B7%E9%97%AE/c65a02d7-3e09-4c53-9761-b59ecbea3513
+@REM 参考项：定位注册表、网页编码
+@REM https://blog.csdn.net/admans/article/details/122682970
+@REM https://blog.csdn.net/lanlangaogao/article/details/122535848
