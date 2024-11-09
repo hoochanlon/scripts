b1041b0b53e14823b5269d6d9edaa8464f276431
123456 <hoochanlon@outlook.com>
2024-11-09 13:46:42 +0800

    fixed

--- d-pwsh-dc/多个共享文件夹授权读写权限.ps1
+++ d-pwsh-dc/多个共享文件夹授权读写权限.ps1
@@ -1,7 +1,7 @@
 # 定义共享名称和文件夹路径
 $shares = @(
     # 定义了一个包含两个共享的数组 $shares。在循环中，我们为每个用户和每个共享依次添加权限。
-    # 这样，用户将同时获得对“生产作业1”和“生产作业2”两个文件夹的访问权限。
+    # 这样，用户将同时获得对“生产作业1”和“生产作业2”两个文件夹的对应权限。
     @{ Name = "生产作业1"; Path = "C:\共享文件夹\生产作业1" },
     @{ Name = "生产作业2"; Path = "C:\共享文件夹\生产作业2" }
 )
@@ -24,7 +24,7 @@ foreach ($user in $domainUsers) {
         # 1. 添加共享权限（只读）
         try {
             Grant-SmbShareAccess -Name $share.Name -AccountName $fullUserName -AccessRight Change -Force
-            Write-Host "共享权限：用户 $fullUserName 已被授予 $($share.Name) 的只读访问权限。"
+            Write-Host "共享权限：用户 $fullUserName 已被授予 $($share.Name) 的编辑权限。"
         } catch {
             Write-Host "共享权限：无法为用户 $fullUserName 添加访问权限到 $($share.Name)，可能该用户已存在或发生其他错误。"
         }
@@ -34,7 +34,7 @@ foreach ($user in $domainUsers) {
             $icaclsCommand = "icacls `"$($share.Path)`" /grant `"${fullUserName}:(OI)(CI)(M)`" /t" # 显示权限
             # 将字符串转换为命令
             Invoke-Expression $icaclsCommand
-            Write-Host "NTFS权限：用户 $fullUserName 已被授予 $($share.Name) 的只读访问权限。"
+            Write-Host "NTFS权限：用户 $fullUserName 已被授予 $($share.Name) 的编辑权限。"
         } catch {
             Write-Host "NTFS权限：无法为用户 $fullUserName 添加访问权限到 $($share.Name)，可能发生错误。"
         }
